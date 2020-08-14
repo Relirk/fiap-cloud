0b1734d60ebfa91549474da315a265a6b5ee1ef8
Relirk <olverajunior2014@gmail.com>
2020-08-14 17:25:43 -0300

    upload https axios

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+import https from "https";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL,
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
 });
 
 export default api;
