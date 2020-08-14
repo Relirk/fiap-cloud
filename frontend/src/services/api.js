5ffb00a37f13fa7c805996dda7e2db0980f3c0b4
Relirk <olverajunior2014@gmail.com>
2020-08-14 19:59:44 -0300

    deploy vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,6 @@
 import axios from "axios";
-import https from "https";
-
-const agent = new https.Agent({
-  rejectUnauthorized: false,
-});
 
 const api = axios.create({
-  httpsAgent: agent,
   baseURL: process.env.REACT_APP_API_URL,
 });
 
